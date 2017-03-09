13f34e03739bfeb7475bfa77c6ecbf530fdd9d18
殷众 <yinzhong@goyoo.com>
2017-03-09 16:50:50 +0800

    update

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ NODE_ENV=production API_PREFIX=$API_PREFIX ./node_modules/.bin/webpack
 
 ./node_modules/.bin/electron-packager . client --ignore=node_modules --ignore=src --ignore=webpack.config.js --ignore=main.dev.js --overwrite
 
-echo "\nDeploy $VERSION success!\n"
+echo "\nDeploy API_PREFIX with $API_PREFIX success!\n"
