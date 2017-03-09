4570d44f288626f630ebcbacc86a5bc53b8b30c0
xiusiteng <xiusiteng@goyoo.com>
2017-03-09 20:38:43 +0800

    article search

--- src/pages/admin/Articles/FormSearch.jsx
+++ src/pages/admin/Articles/FormSearch.jsx
@@ -5,20 +5,55 @@ import style from './style.styl';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
+let timeout;
+let currentValue;
 export default class FormSearch extends Component {
+    state = {
+        data: []
+    }
+    handleChange = (value) => {
+        if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+        }
+        currentValue = value;
+        timeout = setTimeout(async () => {
+            const result = await this.props.searchUser({query: {username: value}});
+            if (currentValue == value) {
+                const data = result.payload.result.data.map((v) => {
+                    return {
+                        value: v.id,
+                        text: v.username
+                    };
+                });
+                this.setState({data});
+            }
+        }, 300);
+    }
     render () {
         const {recentTag, getFieldDecorator, form} = this.props;
         const options = recentTag.map((v, index) => {
             return <Option key={index} value={v}>{v}</Option>;
         });
-
         const children = [
             <div key='1'>
-                <div className={style['form-item-label']}>作者ID</div>
+                <div className={style['form-item-label']}>作者名</div>
                 <FormItem>
                     {getFieldDecorator('author', {
                         initialValue: form.author || ''
-                    })(<Input />)}
+                    })(<Select
+                        combobox
+                        notFoundContent=''
+                        defaultActiveFirstOption={false}
+                        showArrow={false}
+                        filterOption={false}
+                        optionLabelProp='value'
+                        onChange={this.handleChange}
+                    >
+                        {this.state.data.map((d, i) => {
+                            return <Option key={i} value={d.text}> {d.text} </Option>;
+                        })}
+                    </Select>)}
                 </FormItem>
             </div>,
             <div key='2'>
